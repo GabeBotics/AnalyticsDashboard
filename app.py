6a883cde0f126f9f1e466e637d424045048aa68d
GabeBotics <36977341+GabeBotics@users.noreply.github.com>
2023-11-22 20:58:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def img_to_bytes(img_path):
 
 def cs_sidebar():
 
-    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
+    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=148 height=148>](https://github.com/GabeBotics/AnalyticsDashboard)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
     st.sidebar.header('BUS 458 Final Dashboard')
 
     st.sidebar.markdown('''
